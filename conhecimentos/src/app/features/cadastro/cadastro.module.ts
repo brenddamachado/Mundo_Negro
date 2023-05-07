e803ba07747c120c0a96e2d7180e164f3bf07ee6
brenddamachado <brenda16machado.bm@gmail.com>
2023-05-07 12:58:14 -0300

    'cadastro/login'

---
+++ conhecimentos/src/app/features/cadastro/cadastro.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { CadastroComponent } from './pages/cadastro/cadastro.component';
+
+
+
+@NgModule({
+  declarations: [
+    CadastroComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class CadastroModule { }
