55f9fd27abdb7cd312371437e6977ddf0cf16bc2
brenddamachado <brenda16machado.bm@gmail.com>
2023-05-10 22:18:10 -0300

    router

--- conhecimentos/src/app/features/compras/compras.module.ts
+++ conhecimentos/src/app/features/compras/compras.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ComprasComponent } from './pages/compras/compras.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    ComprasComponent
+  ],
   imports: [
     CommonModule
   ]
---
+++ conhecimentos/src/app/features/compras/pages/compras/compras.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  templateUrl: './compras.component.html',
+  styleUrls: ['./compras.component.scss']
+})
+export class ComprasComponent {
+
+}
